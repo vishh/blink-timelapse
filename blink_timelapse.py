32085bc7f004834fe911f5e0c9b6c46b17a465b6
Vish Kannan <vishnuk@vishh.roam.corp.google.com>
2019-12-01 10:06:22 -0800

    remove unwanted logging

--- blink_timelapse.py
+++ blink_timelapse.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python3
 import cv2
 from datetime import datetime, timedelta
+import logging as log
 import os
 import pytz
 import sys
@@ -13,36 +14,38 @@ def now():
  return pytz.timezone('US/Pacific').localize(datetime.now())
 
 def get_sunrise_sunset():
-  city = 'San Francisco'
-  a = Astral()
-  a.solar_depression = 'civil'
-  city = a[city]
-  sun = city.sun(date=datetime.now()+timedelta(days=1), local=True)
-  return sun['sunrise'], sun['sunset']
+    city = 'San Francisco'
+    a = Astral()
+    a.solar_depression = 'civil'
+    city = a[city]
+    sun = city.sun(date=datetime.now(), local=True)
+    if now() >= sun['sunset']:
+        sun = city.sun(date=datetime.now()+timedelta(days=1), local=True)
+    return sun['sunrise'], sun['sunset']
 
 def get_blink_module(username, password):
     blink = blinkpy.Blink(username=username, password=password, refresh_rate=15)
     blink.start()
     return blink
 
-def snap_pictures(path_prefix, blink):
+def snap_pictures(path_prefix, blink, sleep_between):
     for name, camera in blink.cameras.items():
         camera.snap_picture()
         blink.refresh()
         ts = now().strftime('%H_%M_%S')
         output_file = "{}/{}.jpg".format(os.path.join(path_prefix, name), ts)
         camera.image_to_file(output_file)
-        time.sleep(5)
+        time.sleep(sleep_between)
 
 def sleep_until_sunrise():
   sunrise, sunset = get_sunrise_sunset()
   while True:
       ts = now()
       if ts > sunrise:
-        print('Current time is after sunrise')
+        log.info('Current time is after sunrise')
         return
       time_to_sleep = (sunrise-ts).total_seconds()
-      print('Sleeping until sunrise for {} seconds'.format(time_to_sleep))
+      log.info('Sleeping until sunrise for {} seconds'.format(time_to_sleep))
       time.sleep(time_to_sleep)
 
 def create_output_dirs(path_prefix, blink):
@@ -57,18 +60,20 @@ def daytime_timelapse(output_path_prefix, frequency, blink):
   sunrise, sunset = get_sunrise_sunset()
   path_prefix = create_output_dirs(output_path_prefix, blink)
   if now() <= sunrise:
-    print("Current time {} is before sunrise {}".format(now(), sunrise))
+    log.info("Current time {} is before sunrise {}".format(now(), sunrise))
     #return path_prefix
-  print('starting daytime timelapse capture')
+  log.info('starting daytime timelapse capture')
   printcounter = 0
+  per_camera_sleep = min(frequency/len(blink.cameras), 30)
+  return path_prefix
   while True:
     if now() >= sunset:
        break
-    snap_pictures(path_prefix, blink)
+    snap_pictures(path_prefix, blink, per_camera_sleep)
     printcounter += 1
     if (printcounter%1000) == 0:
-      print("Taking pictures")
-    time.sleep(frequency)
+      log.info("Taking pictures")
+    time.sleep(per_camera_sleep)
   return path_prefix
 
 def create_timelapse_video(output_path_prefix, blink):
@@ -76,14 +81,17 @@ def create_timelapse_video(output_path_prefix, blink):
         process_images(os.path.join(output_path_prefix, name))
 
 def process_images(image_dir):
-    video_file_name = "{}.avi".format(os.path.basename(image_dir))
+    video_file_name = "{}.mp4".format(os.path.basename(image_dir))
     video_file_path = os.path.join(image_dir, "../", video_file_name)
-    print('Storing images under {} at {}'.format(image_dir, video_file_path))
+    log.info('Storing images under {} at {}'.format(image_dir, video_file_path))
+
     images = [img for img in os.listdir(image_dir) if img.endswith(".jpg")]
+    images = sorted(images)
+
     frame = cv2.imread(os.path.join(image_dir, images[0]))
     height, width, layers = frame.shape
-
-    video = cv2.VideoWriter(video_file_path, 0, 1, (width,height))
+    fourcc = cv2.VideoWriter_fourcc('a','v','c','1')
+    video = cv2.VideoWriter(video_file_path, fourcc, 12, (width,height))
 
     for image in images:
         video.write(cv2.imread(os.path.join(image_dir, image)))
@@ -92,39 +100,40 @@ def process_images(image_dir):
     video.release()
 
 def get_input_args():
- username = os.environ['USERNAME']
- password = os.environ['PASSWORD']
- frequency = int(os.environ['FREQ_SECONDS'])
- output_dir = os.environ['OUTPUT_PATH']
- if username == "":
-  print('Set USERNAME environment variable')
-  return []
- if password == "":
-  print('Set PASSWORD environment variable')
-  return []
- if frequency <= 0:
-  print('Set FREQ_SECONDS environment variable')
-  return []
- if output_dir == "":
-  print('Set OUTPUT_DIR environment variable')
-  return []
- return [username, password, frequency, output_dir]
+    username = os.environ['USERNAME']
+    password = os.environ['PASSWORD']
+    frequency = int(os.environ['FREQ_SECONDS'])
+    output_dir = os.environ['OUTPUT_PATH']
+    if username == "":
+        log.error('Set USERNAME environment variable')
+        return []
+    if password == "":
+        log.error('Set PASSWORD environment variable')
+        return []
+    if frequency <= 0:
+        log.error('Set FREQ_SECONDS environment variable')
+        return []
+    if output_dir == "":
+        log.error('Set OUTPUT_DIR environment variable')
+        return []
+    return [username, password, frequency, output_dir]
 
 def main():
- inputs = get_input_args()
- if len(inputs) != 4:
-  sys.exit(-1)
+    log.basicConfig(format='%(asctime)s - %(message)s', level=log.INFO)
+    inputs = get_input_args()
+    if len(inputs) != 4:
+        sys.exit(-1)
 
- blink = get_blink_module(inputs[0], inputs[1])
- # In an infinite for loop
- # sleep until it's sunrise
- # take pictures until sunset
- # process the pictures
- # go back to sleeping
- while True:
-   output_path_prefix = daytime_timelapse(inputs[3], inputs[2], blink)
-   create_timelapse_video(output_path_prefix, blink)
-   sleep_until_sunrise()
+    blink = get_blink_module(inputs[0], inputs[1])
+     # In an infinite for loop
+     # sleep until it's sunrise
+     # take pictures until sunset
+     # process the pictures
+     # go back to sleeping
+    while True:
+        output_path_prefix = daytime_timelapse(inputs[3], inputs[2], blink)
+        create_timelapse_video(output_path_prefix, blink)
+        sleep_until_sunrise()
 
 if __name__ == "__main__":
   main()
